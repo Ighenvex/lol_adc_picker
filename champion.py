0b655ca08d42e0fff3ca59099068c432b334d82f
hczhang <96082756+Ighenvex@users.noreply.github.com>
2023-09-06 20:55:14 -0400

    Update champion.py
    Now no longer opens two chrome instances, and does both searches in one chrome instance, reducing the runtime.

--- champion.py
+++ champion.py
@@ -5,27 +5,13 @@ from selenium.webdriver.chrome.options import Options
 from scipy.stats import binom
 import pprint as pp
 
-adcs = {"seraphine", "nilah", "kalista", "twitch", "ashe", "aphelios", "vayne", 
-        "kog'maw", "draven", "yasuo", "jhin", "zeri", "sivir", "lucian", "xayah",
-        "samira", "miss fortune", "ezreal", "jinx", "kai'sa", "tristana", "caitlyn", 
-        "varus", "swain", "ziggs"}
-
 def process_matches(matches: str):
     """Will get rid of commas and return an int"""
     return int(matches.replace(",", ""))
 
-def scrape_link(url, ally):
-    """ally is a bool"""
-    options = Options()
-    options.add_argument("--headless") # Run Chrome in headless mode
-    options.add_argument('log-level=3') # Hides non-essential warnings
-    options.add_argument('--ignore-ssl-errors')
-    options.add_argument('--ignore-certificate-errors')
-    service = Service("chromedriver.exe") # Path to Chromedriver executable
-    driver = webdriver.Chrome(service=service, options=options)
-    # driver.implicitly_wait(1)
+def scrape_link(url, ally, driver):
     driver.get(url)
-
+    
     rows = driver.find_elements(By.CSS_SELECTOR, "div.rt-tr-group")
     if ally: # This is just for setting values used in getting the data
         x, y, z = 3, 4, 10
@@ -43,14 +29,6 @@ def scrape_link(url, ally):
             output[champion] = round(1 - binom.cdf(round(wr * matches), matches, 0.5), 5)
     return output
 
-def get_counterpicks(opp_adc):
-    res = scrape_link(f"https://u.gg/lol/champions/{opp_adc}/matchups", False)
-    return res
-
-def get_duos(ally_supp):
-    res = scrape_link(f"https://u.gg/lol/champions/{ally_supp}/duos", True)
-    return res
-
 def _pprint_dict(self, object, stream, indent, allowance, context, level):
     """
     Overwrites the default pprint for dictionaries to remove the automatic
@@ -65,13 +43,22 @@ def _pprint_dict(self, object, stream, indent, allowance, context, level):
         self._format_dict_items(object.items(), stream, indent, allowance + 1,
                                 context, level)
     write('}')
-
 pp.PrettyPrinter._dispatch[dict.__repr__] = _pprint_dict
 
 enemy_adc = input("Enemy ADC: ")
 ally_supp = input("Ally Support: ")
-counters = get_counterpicks(enemy_adc)
-duos = get_duos(ally_supp)
+options = Options()
+options.add_argument("--headless") # Run Chrome in headless mode
+options.add_argument('log-level=3') # Hides non-essential warnings
+options.add_argument('--ignore-ssl-errors')
+options.add_argument('--ignore-certificate-errors')
+service = Service(r"chromedriver.exe") # Path to Chromedriver executable
+driver = webdriver.Chrome(service=service, options=options)
+driver.implicitly_wait(1)
+
+counters = scrape_link(f"https://u.gg/lol/champions/{enemy_adc}/matchups", False, driver)
+duos = scrape_link(f"https://u.gg/lol/champions/{ally_supp}/duos", True, driver)
+driver.quit()
 
 final_ratings = {x: round(50 * (counters.get(x, 0) + duos.get(x, 0)), 5)
                 for x in set(counters).intersection(duos)}
@@ -85,3 +72,4 @@ not_matched.remove(enemy_adc.title())
 print()
 pp.pprint(sorted_ratings)
 print(f"\nUnable to match: {not_matched}")
+input(f"\nPress Enter to exit.")
